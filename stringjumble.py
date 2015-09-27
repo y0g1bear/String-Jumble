bc3ea89cc0ef3ed6b556acd067476d1263559ea4
Eric Dennison <ericd@netdenizen.com>
2015-09-27 14:02:17 -0400

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -5,3 +5,23 @@ Credit: <list sources used, if any>
 
 Assignment:
 
+The purpose of this challenge is to gain proficiency with 
+manipulating lists.
+
+Write and submit a Python program that accepts a string from 
+the user and prints it back in three different ways:
+
+* With all letters in reverse.
+* With words in reverse order, but letters within each word in 
+  the correct order.
+* With all words in correct order, but letters reversed within 
+  the words.
+
+Output of your program should look like this:
+
+Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
+You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
+ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
+handy find may you that tricks or techniques few a are There
+erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
+"""
