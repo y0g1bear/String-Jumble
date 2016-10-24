8e34668266ddfbdd5ca3467b895de78059914f31
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-10-24 13:51:14 -0400

    Updated from Brython Server: 10/24/2016 1:51:13 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,11 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+we = input("Please enter a string of text (the bigger the better):")
+wee = ""
+print("You entered "+ we + ". Now jumble it:")
+for i in reversed(we):
+    print(i)
+    
+
