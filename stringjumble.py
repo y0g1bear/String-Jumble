b9ec7997ca84d084eb91bc4d4f1300f427cb38eb
y0g1bear <johnwarhold@hanovernorwichschools.org>
2016-10-25 13:51:29 -0400

    Updated from Brython Server: 10/25/2016 1:51:29 PM

--- stringjumble.py
+++ stringjumble.py
@@ -29,8 +29,7 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 we = input("Please enter a string of text (the bigger the better):")
 print("You entered "+ we + ". Now jumble it:")
 for i in reversed(we):
-    print(i, end = " ")
-for l in -1(we):
-    print(l, end = " ")
-    
-
+    print(i, end = "")
+we = list(we)
+print( )
+print(we)
